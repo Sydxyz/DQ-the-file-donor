17634d0d6ad88d766342d7bd3879b601bdd2c205
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-06-01 08:00:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1294,7 +1294,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InputMediaPhoto(random.choice(PICS))
         )
         await query.message.edit_text(
-            text=script.SOURCE_TXT,
+            text=script.SOURCE_TXT.format(query.from_user.id, query.from_user.last_name),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
