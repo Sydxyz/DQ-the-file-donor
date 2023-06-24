ca5f51de9bf642cac5088ca815a602f08b45b0ee
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-06-24 16:04:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -103,7 +103,7 @@ async def next_page(bot, query):
     if 'is_shortlink' in settings.keys():
         ENABLE_SHORTLINK = settings['is_shortlink']
     else:
-       await save_group_settings(query.message.chat.id, 'is_shortlink', False)
+        await save_group_settings(query.message.chat.id, 'is_shortlink', False)
         ENABLE_SHORTLINK = False
     if ENABLE_SHORTLINK and settings['button']:
         btn = [
